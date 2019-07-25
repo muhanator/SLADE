1c4ebdc6bea96ea6c3fc866e39058cbebea2dff2
Jeremy Davis <jeremy.davis@mail.mcgill.ca>
2019-07-24 21:40:05 -0400

    Created example for adding tasks to the Today View at runtime

--- app/src/main/java/com/example/sladetest/MainActivity.kt
+++ app/src/main/java/com/example/sladetest/MainActivity.kt
@@ -12,12 +12,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
 import android.content.Intent
+import android.view.Gravity
 import android.view.View
 import android.widget.*
 import java.util.Calendar
+import android.view.ViewGroup.LayoutParams
+import android.graphics.Color
 
 
 import java.text.DateFormat
+import android.widget.LinearLayout
+import android.widget.TextView
+
+
+
+
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -48,18 +57,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         //Below, we initilize the timetable scrollview
         val timetableView = findViewById<ScrollView>(R.id.timetable_scrollview)
-        timetableView.post(Runnable { timetableView.scrollTo(0, 250) })
 
         //Below, we initialize the timetable time indicator so that it can move depending on the time of the day
         val  timetable_indicator_mover = findViewById<ImageView>(R.id.timetable_indicator_mover)
         val  hour = (calendar.get(Calendar.HOUR_OF_DAY)).toDouble()
         val  minute = calendar.get(Calendar.MINUTE).toDouble()
         val scale = resources.displayMetrics.density
-        val dpHeightInPx = ((118 + 80 * (hour + (minute/60.toDouble()))) * scale).toInt()     //calculation: ((start of 12AM row + rowHeight * ( hour + minute/60) )
+        val dpHeightInPx = dpToPx((118 + 80 * (hour + (minute/60.toDouble()))))   //calculation: ((start of 12AM row + rowHeight * ( hour + minute/60) )
         timetable_indicator_mover.layoutParams.height = dpHeightInPx
         timetable_indicator_mover.visibility = View.INVISIBLE
 
-
+        //Below, we update the schedule view with all of the tasks for the given day
+        updateScheduleView()
 
         //Below, we initialize the action toolbar
         val toolbar: Toolbar = findViewById(R.id.toolbar)
@@ -137,6 +146,65 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    fun updateScheduleView(){
+
+        //Create a task button
+        /* working
+        val taskButton = ImageButton(this)
+        taskButton.scaleType = ImageView.ScaleType.FIT_XY
+        taskButton.setBackgroundColor(Color.parseColor("#00FFFFFF"))
+        taskButton.layoutParams = LinearLayout.LayoutParams(dpToPx(150.0),dpToPx(160.0))
+        //taskButton.foregroundGravity(Gravity.START)
+        taskButton.setImageResource(R.drawable.task_icon)
+        //taskButton.paddingTop = 100 */
+
+
+        //Create button #1
+        val taskButton = Button(this)
+        taskButton.setBackgroundColor(Color.parseColor("#00FFFFFF"))
+        var params = LinearLayout.LayoutParams(dpToPx(150.0),dpToPx(160.0))
+        params.setMargins(0, dpToPx(80.0), 0, 0)
+        taskButton.layoutParams = params
+        taskButton.setBackgroundResource(R.drawable.task_icon)
+        taskButton.setText("Task #1: Do the thing.")
+
+        //Create button #2
+        val taskButton2 = Button(this)
+        taskButton2.setBackgroundColor(Color.parseColor("#00FFFFFF"))
+        var params2 = LinearLayout.LayoutParams(dpToPx(150.0),dpToPx(160.0))
+        params2.setMargins(0, dpToPx(160.0), 0, 0)
+        taskButton2.layoutParams = params2
+        taskButton2.setBackgroundResource(R.drawable.task_icon)
+        taskButton2.setText("Task #2: Do the other thing.")
+
+        //Create a new LinearLayout
+        val child = LinearLayout(this)
+        child.setPadding(dpToPx(70.0), dpToPx(125.0), 0, 0)
+        child.layoutParams = LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        child.addView(taskButton)
+
+        //Create a new LinearLayout 2
+        val child2 = LinearLayout(this)
+        child2.setPadding(dpToPx(230.0), dpToPx(125.0), 0, 0)
+        child2.layoutParams = LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        child2.addView(taskButton2)
+
+        //Add the linear layout to the frame layout
+        val frameLayout = findViewById<FrameLayout>(R.id.schedule_frame_layout)
+        frameLayout.addView(child)
+        frameLayout.addView(child2)
+    }
+
+
+    fun dpToPx(size : Double): Int{
+
+        val scale = resources.displayMetrics.density
+
+        val dpHeightInPx = (size * scale + 0.5f).toInt()
+
+        return dpHeightInPx
     }
 
 
