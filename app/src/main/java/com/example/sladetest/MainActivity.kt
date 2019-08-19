5f69aed7414208fd012c3d2abd6807d224633de0
Jeremy Davis <jeremy.davis@mail.mcgill.ca>
2019-08-19 18:47:16 -0400

    Task-17: Table row height can now be controlled by a single variable

--- app/src/main/java/com/example/sladetest/MainActivity.kt
+++ app/src/main/java/com/example/sladetest/MainActivity.kt
@@ -1,7 +1,4 @@
 package com.example.sladetest
-
-
-import android.content.Context
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -12,14 +9,14 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.content.Intent
-import android.content.res.Resources
-import android.graphics.Color
 import android.view.*
 import android.widget.*
 import java.text.DateFormat
 import android.widget.TextView
 import java.util.*
 import java.time.LocalDate
+import android.view.ScaleGestureDetector
+import androidx.core.view.children
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -28,6 +25,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     var year : Int? = 0
     var month: Int? = 0
     var day  : Int? = 0
+    private var tableRowHeight = 80
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //This function runs once upon creation of the activity
@@ -42,7 +41,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // If the activity is being called from CalendarActivity, it will use the given date which is not necessarily today's.
         initializeDate()
 
-      
         //Below, we initialize the progress bar
         val progressBar = findViewById<ProgressBar>(R.id.progressBar)
         progressBar.progress = 50
@@ -50,21 +48,32 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
 
         //Below, we initialize the timetable time indicator (red line) so that it can move depending on the time of the day
-        val timetableIndicatorMover = findViewById<ImageView>(R.id.timetable_indicator_mover)
-        val hour                    = calendar.get(Calendar.HOUR_OF_DAY).toDouble()
-        val minute                  = calendar.get(Calendar.MINUTE).toDouble()
-        val dpHeightInPx            = dpToPx((118 + 80 * (hour + (minute/60.toDouble())))) //calculation: ((start of 12AM row + rowHeight * ( hour + minute/60) )
-
-        timetableIndicatorMover.layoutParams.height = dpHeightInPx
-        timetableIndicatorMover.visibility          = View.INVISIBLE
+        var timetableIndicatorMover = findViewById<ImageView>(R.id.timetable_indicator_mover)
+        updateTimeIndicator(timetableIndicatorMover)
 
 
         //Below, we update the schedule view with all of the tasks for the given day
         val frameLayout = findViewById<FrameLayout>(R.id.task_icon_container)
-        val timeTableRow = findViewById<TableRow>(R.id.tableRow_12am)
+        val timeTable = findViewById<TableLayout>(R.id.task_timeTable)
+
+        for(tableRow in timeTable.children) {
+
+            if(tableRow.id == R.id.tableRow_date)        {continue} // We do not want to expand the height of the table rows
+            if(tableRow.id == R.id.tableRow_progressText){continue} // that contain the date, and progress bar, which are also
+            if(tableRow.id == R.id.tableRow_progressBar) {continue} // present in the same table
+
+
+            tableRow.minimumHeight = dpToPx(tableRowHeight.toDouble())
+            updateTimeIndicator(timetableIndicatorMover)
+        }
 
 
 
+        /*
+         android:layout_width="169dp" android:layout_height="80dp"
+         android:background="@drawable/table_border"
+         android:minHeight="80dp" android:id="@+id/tableRow_12am"
+        */
 
         val content = findViewById<View>(android.R.id.content)
         content.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
@@ -75,10 +84,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 content.viewTreeObserver.removeGlobalOnLayoutListener(this)
                 //Now you can get the width and height from content
 
-                TaskManager.updateTodayView(year!!, month!!, day!!, frameLayout, timeTableRow.measuredHeight, this@MainActivity)
+                TaskManager.updateTodayView(year!!, month!!, day!!, frameLayout, dpToPx(tableRowHeight.toDouble()), this@MainActivity)
 
             }
         })
+
 
         //Below, we initialize the action toolbar
         val toolbar: Toolbar = findViewById(R.id.toolbar)
@@ -92,6 +102,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             intent = Intent(this, TaskCreateActivity::class.java)
             startActivity(intent)
         }
+
+
+        //Below, we initialize the Sidebar
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val toggle = ActionBarDrawerToggle(
@@ -102,8 +115,25 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         navView.setNavigationItemSelectedListener(this)
 
+    }
 
 
+
+
+    //Below, we initialize pinch-to-zoom
+    private var mScaleFactor = 1f
+
+    private val scaleListener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            mScaleFactor *= detector.scaleFactor
+
+            // Don't let the object get too small or too large.
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f))
+
+            //invalidate()
+            return true
+        }
     }
 
     fun initializeDate(){
@@ -178,6 +208,21 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
             R.id.nav_share -> {
 
+                val task        = TaskManager.createTask(2019, 8, 15, 20, 0, 22, 0, "1"  , 1)
+                val task2       = TaskManager.createTask(2019, 8, 15, 8 , 0, 10, 0, "2"  , 2)
+                val task3       = TaskManager.createTask(2019, 8, 15, 3 , 15, 6 , 45, "3", 3)
+                val task4       = TaskManager.createTask(2019, 8, 15, 1 , 0, 2 , 0, "4"  , 4)
+                val task5       = TaskManager.createTask(2019, 8, 15, 2 , 0, 4 , 0, "1"  , 5)
+                val task6       = TaskManager.createTask(2019, 8, 15, 2 , 0, 4 , 0, "2"  , 6)
+                val task7       = TaskManager.createTask(2019, 8, 15, 2 , 0, 4 , 0, "1"  , 7)
+                val task8       = TaskManager.createTask(2019, 8, 15, 2 , 0, 4 , 0, "2"  , 8)
+                val task9       = TaskManager.createTask(2019, 8, 15, 5 , 0, 6 , 0, "2"  , 9)
+                val task10      = TaskManager.createTask(2019, 8, 15, 0 , 0, 1 , 0, "2"  , 10)
+                task.setTaskDescription("This task was made created using task manager")
+                task2.setTaskDescription("This task2 was made created using task manager")
+                task3.setTaskDescription("This task3 was made created using task manager")
+                task4.setTaskDescription("This task4 was made created using task manager")
+
             }
             R.id.nav_send -> {
 
@@ -193,6 +238,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val scale = resources.displayMetrics.density
 
         return (size * scale + 0.5f).toInt()
+    }
+
+    private fun updateTimeIndicator(timetableIndicatorMover: ImageView){
+
+        val hour                    = calendar.get(Calendar.HOUR_OF_DAY).toDouble()
+        val minute                  = calendar.get(Calendar.MINUTE).toDouble()
+        val dpHeightInPx            = dpToPx((118 + tableRowHeight * (hour + (minute/60.toDouble())))) //calculation: ((start of 12AM row + rowHeight * ( hour + minute/60) )
+
+        timetableIndicatorMover.layoutParams.height = dpHeightInPx
+        timetableIndicatorMover.visibility          = View.INVISIBLE
     }
 
 
